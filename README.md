656ea1e51fb85f9a0ac1d76421256575e460cdb6
HHR <467178874@qq.com>
2020-01-22 21:09:56 +0800

    添加config文件保存启动参数

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # ZJUT-Scorer
 ZJUT自动查分+通知  
 自动查询你的期末考成绩并用邮件通知  
-使用了[zjutjh](https://github.com/zjutjh)提供的[API](https://github.com/zjutjh/document/blob/master/5-API/5.4-%E6%AD%A3%E6%96%B9%E6%88%90%E7%BB%A9.md)
+使用了[zjutjh](https://github.com/zjutjh)提供的[API](https://github.com/zjutjh/document/blob/master/5-API/5.4-%E6%AD%A3%E6%96%B9%E6%88%90%E7%BB%A9.md)  
+---
+注意，请在同目录放置config.ini,内容是  
++ 学号  
++ 正方教务密码
++ 学年
++ 学期(3为上学期，12为下学期, 16为短学期)
++ SMTP服务器(如 smtp.qq.com)
++ 发件邮箱
++ 发件邮箱密码
++ 收件邮箱
--- main.py
+++ main.py
@@ -8,15 +8,15 @@ from email.mime.text import MIMEText
 from email.header import Header
 
 
-def sendmail(msg):
-    mail_host = "smtp.qq.com"
-    mail_user = "XX@XX.XX"
-    mail_pass = "XXXX"
-    sender = 'XX@XX.XX'
-    receivers = ['XX@XX.XX']
+def sendmail(msg, config):
+    mail_host = config[4]
+    mail_user = config[5]
+    mail_pass = config[6]
+    sender = config[5]
+    receivers = config[7]
     message = MIMEText(msg)
     message['From'] = Header('S-Server<score@zjut.edu.cn>', 'utf-8')
-    message['To'] = Header('XX@XX.XX<XX@XX.XX>', 'utf-8')
+    message['To'] = Header(config[7] + '<' + config[7] + '>', 'utf-8')
     message['Subject'] = Header('出成绩了！', 'utf-8')
     try:
         SMTPObj = smtplib.SMTP_SSL(mail_host, 465)
@@ -24,18 +24,20 @@ def sendmail(msg):
         SMTPObj.login(mail_user, mail_pass)
         SMTPObj.sendmail(sender, receivers, message.as_string())
         print("邮件发送成功")
-    except smtplib.SMTPException:
+    except BufferError:
         print("Error: 无法发送邮件")
 
 
 if __name__ == '__main__':
+    configFile = open('config.ini', mode='r', encoding='utf-8')
+    config = configFile.read().split('\n')
     headers = {
         "Host": "api.jh.zjut.edu.cn",
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
                       "Chrome/79.0.3945.88 Safari/537.36 Edg/79.0.309.54 "
     }
-    # TODO
-    url = "http://api.jh.zjut.edu.cn/student/scoresZf.php?username=XXXXXXXXXXXX&password=XXXXXX&year=2019&term=3"
+    url = "http://api.jh.zjut.edu.cn/student/scoresZf.php?username=" + config[0] + "&password=" + config[1] + "&year=" + \
+          config[2] + "&term=" + config[3]
     size = 0  # 现在已经出成绩的科目数量
     flag = 0  # 标记有没有成功查到过成绩
     while True:
@@ -56,10 +58,17 @@ if __name__ == '__main__':
                     continue
             flag = 1
             if len(decodedJSON["msg"]) != size:
+                credit = 0
+                GP = 0
                 msg = ''
                 for each in decodedJSON["msg"]:
+                    if each['kcxzmc'] != '任选课':
+                        credit = credit + eval(each['xf'])
+                        GP = GP + eval(each['xf']) * eval(each['jd'])
                     msg = msg + each['kcmc'] + '\t' + each['classscore'] + '\n'
-                sendmail(msg)
+                GPA = GP / credit
+                msg = msg + "当前GPA：\t" + str(GPA) + '\n'
+                sendmail(msg, config)
                 size = len(decodedJSON["msg"])
             # print(len(decodedJSON["msg"]))
             time.sleep(60)
