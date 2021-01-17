6d2cf9727355e07c19f062cfa73b8e5b23dfb51c
HHR <i@huhaorui.com>
2021-01-17 15:43:36 +0800

    update

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def sendmail(msg, config):
     mail_pass = config[6]
     sender = config[5]
     receivers = config[7]
-    message = MIMEText(msg)
+    message = getHTML(msg)
     message['From'] = Header('S-Server<score@zjut.edu.cn>', 'utf-8')
     message['To'] = Header(config[7] + '<' + config[7] + '>', 'utf-8')
     message['Subject'] = Header('出成绩了！', 'utf-8')
@@ -30,6 +30,43 @@ def sendmail(msg, config):
         print("网络连接错误，请检查SMTP服务器配置")
 
 
+def getHTML(msg):
+    msg = msg.replace('\n', '</td>\n</tr>\n<tr>\n<td>')
+    msg = msg.replace('\t', '</td>\n<td>')
+    html = """
+<html lang="zh-cn">
+<head>
+    <style>
+        tr, td, th, table {
+            border: 1px solid black;
+        }
+    </style>
+</head>
+<body>
+<table>
+    <tr>
+        <th>
+            课程名称
+        </th>
+        <th>
+            成绩
+        </th>
+    </tr>
+    <tr>
+        <td>
+    """ + msg + """
+           </td>
+    </tr>
+</table>
+</body>
+</html>
+    """
+    print(html)
+    text_html = MIMEText(html, 'html', 'utf-8')
+    text_html["Content-Disposition"] = 'attachment; filename="scores.html"'
+    return text_html
+
+
 if __name__ == '__main__':
     configFile = open('config.ini', mode='r', encoding='utf-8')
     config = configFile.read().split('\n')
@@ -38,8 +75,8 @@ if __name__ == '__main__':
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
                       "Chrome/79.0.3945.88 Safari/537.36 Edg/79.0.309.54 "
     }
-    url = "http://api.jh.zjut.edu.cn/student/scoresZf.php?username=" + config[0] + "&password=" + config[1] + "&year=" + \
-          config[2] + "&term=" + config[3]
+    url = "http://api.jh.zjut.edu.cn/student/scoresZf.php?ip=164&username=" + config[0] + "&password=" + config[
+        1] + "&year=" + config[2] + "&term=" + config[3]
     size = 0  # 现在已经出成绩的科目数量
     flag = 0  # 标记有没有成功查到过成绩
     while True:
@@ -47,7 +84,6 @@ if __name__ == '__main__':
             times = 0
             content = requests.get(url, headers=headers).content.decode().encode("GBK")
             decodedJSON = json.loads(content)
-            # print(decodedJSON)
             while decodedJSON["status"] != "success":
                 print('查询错误')
                 content = requests.get(url, headers=headers).content.decode().encode("GBK")
@@ -69,10 +105,9 @@ if __name__ == '__main__':
                         GP = GP + eval(each['xf']) * eval(each['jd'])
                     msg = msg + each['kcmc'] + '\t' + each['classscore'] + '\n'
                 GPA = GP / credit
-                msg = msg + "当前GPA：\t" + str(GPA) + '\n'
+                msg = msg + "当前GPA：\t" + str(GPA)
                 sendmail(msg, config)
                 size = len(decodedJSON["msg"])
-            # print(len(decodedJSON["msg"]))
             time.sleep(60)
         except requests.exceptions.ConnectionError as e:
             print('Network Error')
@@ -80,3 +115,4 @@ if __name__ == '__main__':
             print('KeyError')
         except json.decoder.JSONDecodeError as e:
             print('JsonDecodeError')
+
